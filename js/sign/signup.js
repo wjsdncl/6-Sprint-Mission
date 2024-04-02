59a0ba8f579c81152190d283dbf778dbc2034510
wjsdncl <89517903+wjsdncl@users.noreply.github.com>
2024-04-02 13:14:26 +0900

    fix: signin.js, signup.js 수정

--- js/sign/signup.js
+++ js/sign/signup.js
@@ -1,34 +1,33 @@
 import {
     validateEmail,
-    validateName,
+    validateNickname,
     validatePassword,
     validatePasswordMatch,
 } from "/js/utils/validator.js";
 
 const emailInput = document.getElementById("email");
-const nameInput = document.getElementById("name");
+const nicknameInput = document.getElementById("nickname");
 const passwordInput = document.getElementById("psw");
 const passwordConfirmInput = document.getElementById("psw_chk");
 const submitButton = document.getElementById("btn");
 
 // 입력값이 변경될 때마다 검사
 emailInput.addEventListener("input", validateInputs);
-nameInput.addEventListener("input", validateInputs);
+nicknameInput.addEventListener("input", validateInputs);
 passwordInput.addEventListener("input", validateInputs);
 passwordConfirmInput.addEventListener("input", validateInputs);
 
 function validateInputs() {
     const isValidEmail = validateEmail(emailInput.value);
-    const isValidName = validateName(nameInput.value);
+    const isValidNickname = validateNickname(nicknameInput.value);
     const isValidPassword = validatePassword(passwordInput.value);
     const isPasswordMatch = validatePasswordMatch(
         passwordInput.value,
         passwordConfirmInput.value
     );
-
     submitButton.disabled = !(
         isValidEmail &&
-        isValidName &&
+        isValidNickname &&
         isValidPassword &&
         isPasswordMatch
     );
--- js/utils/validator.js
+++ js/utils/validator.js
@@ -2,8 +2,8 @@ export function validateEmail(email) {
     return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email.trim());
 }
 
-export function validateName(name) {
-    return name.trim().length > 2;
+export function validateNickname(name) {
+    return name.trim().length >= 2;
 }
 
 export function validatePassword(password) {
