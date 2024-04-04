4e14ecf943b1d50764fdaab355acb224bbf922ed
wjsdncl <89517903+wjsdncl@users.noreply.github.com>
2024-04-05 01:57:57 +0900

    feat: 로그인, 회원가입 페이지 수정 - 로그인, 회원가입 페이지에서 유효성 검사 후 유효하지 않다면 에러 메세지 출력 - 로그인, 회원가입 페이지에서 비밀번호 표시/비표시 버튼 추가

--- js/sign/signin.js
+++ js/sign/signin.js
@@ -1,16 +1,63 @@
-import { validateEmail, validatePassword } from "/js/utils/validator.js";
+import {
+  validateEmail,
+  validatePassword,
+  validateInput,
+  togglePasswordVisibility,
+} from "/js/utils/validator.js";
 
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("psw");
+
 const submitButton = document.getElementById("btn");
 
-function validateInputs() {
-    const isValidEmail = validateEmail(emailInput.value);
-    const isValidPassword = validatePassword(passwordInput.value);
+const errorMessages = {
+  email: {
+    empty: "이메일을 입력해주세요.",
+    invalid: "잘못된 이메일 형식입니다.",
+  },
+  password: {
+    empty: "비밀번호를 입력해주세요.",
+    invalid: "비밀번호를 8자 이상 입력해주세요.",
+    match: "비밀번호가 일치하지 않습니다.",
+  },
+};
 
-    submitButton.disabled = !(isValidEmail && isValidPassword);
+// 버튼 활성화를 위한 유효성 검사
+function validateSubmitButton() {
+  const isValidEmail = validateEmail(emailInput.value);
+  const isValidPassword = validatePassword(passwordInput.value);
+
+  submitButton.disabled = !(isValidEmail && isValidPassword);
 }
 
-// 입력값이 변경될 때마다 검사
-emailInput.addEventListener("input", validateInputs);
-passwordInput.addEventListener("input", validateInputs);
+// 입력값이 변경될 때마다 유효성 검사 실행
+emailInput.addEventListener("input", () => {
+  validateInput(emailInput, validateEmail, errorMessages.email),
+    validateSubmitButton();
+});
+passwordInput.addEventListener("input", () => {
+  validateInput(passwordInput, validatePassword, errorMessages.password),
+    validateSubmitButton();
+});
+
+// 포커스를 잃었을 때 유효성 검사 실행
+emailInput.addEventListener("blur", () =>
+  validateInput(emailInput, validateEmail, errorMessages.email)
+);
+passwordInput.addEventListener("blur", () =>
+  validateInput(passwordInput, validatePassword, errorMessages.password)
+);
+
+// 비밀번호 가리기/표시 버튼 클릭 시 비밀번호 가리기/표시 기능 토글
+const passwordToggleButtons = document.querySelectorAll(".psw_chk_btn");
+passwordToggleButtons[0].addEventListener("click", () => {
+  togglePasswordVisibility(passwordInput);
+  const associatedImage =
+    passwordToggleButtons[0].querySelector(".psw_chk_img");
+  associatedImage.classList.toggle("eye_open");
+  associatedImage.classList.toggle("eye_close");
+});
+
+submitButton.addEventListener("click", function () {
+  window.location.href = "/pages/items/index.html";
+});
--- js/utils/validator.js
+++ js/utils/validator.js
@@ -1,15 +1,63 @@
 export function validateEmail(email) {
-    return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email.trim());
+  return /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(email.trim());
 }
 
 export function validateNickname(name) {
-    return name.trim().length >= 2;
+  return name.trim().length >= 2;
 }
 
 export function validatePassword(password) {
-    return password.trim().length >= 8;
+  return password.trim().length >= 8;
 }
 
 export function validatePasswordMatch(password, confirmPassword) {
-    return password === confirmPassword;
+  return password === confirmPassword;
+}
+
+function showErrorMessage(input, message) {
+  const errorMessageElement = input.parentNode.querySelector(".error_msg");
+  errorMessageElement.innerText = message;
+  errorMessageElement.classList.remove("hide");
+  input.classList.add("error_line");
+}
+
+function hideErrorMessage(input) {
+  const errorMessageElement = input.parentNode.querySelector(".error_msg");
+  errorMessageElement.classList.add("hide");
+  input.classList.remove("error_line");
+}
+
+export function validateInput(input, validationFunction, errorMessage) {
+  if (input.value === "") {
+    showErrorMessage(input, errorMessage.empty);
+    return false;
+  } else if (!validationFunction(input.value)) {
+    showErrorMessage(input, errorMessage.invalid);
+    return false;
+  } else {
+    hideErrorMessage(input);
+    return true;
+  }
+}
+
+export function validatePasswordMatchInput(
+  pswInput,
+  pswMatchInput,
+  errorMessage
+) {
+  const isPasswordMatch = validatePasswordMatch(
+    pswInput.value,
+    pswMatchInput.value
+  );
+
+  if (!isPasswordMatch) {
+    showErrorMessage(pswMatchInput, errorMessage);
+  } else {
+    hideErrorMessage(pswMatchInput);
+  }
+}
+
+// 비밀번호 가리기/표시
+export function togglePasswordVisibility(input) {
+  input.type = input.type === "text" ? "password" : "text";
 }
